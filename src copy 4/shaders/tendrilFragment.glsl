685869a1ce9894f1824ccae5eee1eddf2d57106f
VortexBlack <matthewnardizzi@Matthews-MacBook-Air.local>
2025-05-23 17:35:10 -0400

    WIP – backup before clean scaffold

--- src copy 4/shaders/tendrilFragment.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 vUv;
-
-void main() {
-  float intensity = 1.0 - length(vUv - 0.5);
-  intensity = pow(intensity, 3.0); // soft glowing core
-
-  vec3 glowColor = vec3(0.0, 1.0, 1.0); // cyan
-  gl_FragColor = vec4(glowColor * intensity, intensity);
-}
--- src copy 5/shaders/vertexShader.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 vUv;
-uniform float uTime;
-
-void main() {
-  vUv = uv;
-  vec3 pos = position;
-  pos.x += sin(uTime + pos.y * 4.0) * 0.15;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-}

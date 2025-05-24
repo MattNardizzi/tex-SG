f071dbc1d11d22746b2e52bcf3de1db2b7556778
VortexBlack <matthewnardizzi@Matthews-MacBook-Air.local>
2025-05-24 18:50:37 -0400

    Fix layout sizing for consistent Canvas and UI on Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,22 @@ import SovereignTextbox from '@/components/ui/SovereignTextbox';
 
 export default function Home() {
   return (
-    <main className="relative h-screen w-screen bg-black font-grotesk overflow-hidden">
+    <main className="relative min-h-screen w-screen bg-black font-grotesk overflow-hidden">
       {/* Mutation Log Panel (self-positioned) */}
       <MutationLogPanel />
 
       {/* Sovereign Status Panel (self-positioned) */}
       <SovereignStatusPanel />
 
-      {/* Central Canvas */}
-      <div className="absolute inset-0 flex items-center justify-center">
-        <SpineCanvas className="h-[90vh] w-full max-w-[90vw]" />
+      {/* Central Spine Canvas */}
+      <div className="absolute inset-0 flex items-center justify-center z-0">
+        <div className="w-full max-w-[90vw] h-[90vh]">
+          <SpineCanvas className="w-full h-full" />
+        </div>
       </div>
 
       {/* Bottom-Centered Textbox */}
-      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 w-[400px]">
+      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10 w-[90%] max-w-[420px]">
         <SovereignTextbox />
       </div>
     </main>

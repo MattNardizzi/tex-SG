623f1bdd2770ddf963c70cbda31a697adffe56e1
VortexBlack <matthewnardizzi@Matthews-MacBook-Air.local>
2025-05-24 19:44:52 -0400

    Update page layout for Vercel compliance and fixed panel positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,18 @@ export default function Home() {
   return (
     <main className="relative h-screen w-screen bg-black font-grotesk overflow-hidden">
 
-      {/* Mutation Log Panel (150px left of center) */}
-      <MutationLogPanel
-        className="absolute top-12 left-[calc(50%-150px)]"
-      />
+      {/* Mutation Log Panel – 150px left of center */}
+      <MutationLogPanel className="absolute top-12 left-[calc(50%-150px)]" />
 
-      {/* Sovereign Status Panel (150px right of center) */}
-      <SovereignStatusPanel
-        className="absolute top-12 left-[calc(50%+150px)] -translate-x-full"
-      />
+      {/* Sovereign Status Panel – 150px right of center */}
+      <SovereignStatusPanel className="absolute top-12 left-[calc(50%+150px)] -translate-x-full" />
 
-      {/* Central Canvas */}
+      {/* Central 3D Canvas */}
       <div className="absolute inset-0 flex items-center justify-center">
         <SpineCanvas className="h-[90vh] w-full max-w-[90vw]" />
       </div>
 
-      {/* Bottom-Centered Textbox */}
+      {/* Textbox at bottom center */}
       <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 w-[400px]">
         <SovereignTextbox />
       </div>

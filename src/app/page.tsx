b6d643d23f8fe44745f2fb6d5d3340bb1a31f815
VortexBlack <matthewnardizzi@Matthews-MacBook-Air.local>
2025-05-24 20:36:04 -0400

    Remove TexScene import, use SpineCanvas instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { Canvas } from '@react-three/fiber';
-import TexScene from '../components/_3d/TexScene';
+import SpineCanvas from '../components/Spine/SpineCanvas';
 import MutationLogPanel from '../components/panels/MutationLogPanel';
 import SovereignStatusPanel from '../components/panels/SovereignStatusPanel';
 import SovereignTextbox from '../components/ui/SovereignTextbox';
@@ -9,25 +8,18 @@ import SovereignTextbox from '../components/ui/SovereignTextbox';
 export default function MainPage() {
   return (
     <div className="relative w-full h-screen bg-black font-grotesk overflow-hidden">
-      {/* 3D Canvas */}
-      <Canvas>
-        <TexScene />
-      </Canvas>
+      {/* R3F Canvas Scene */}
+      <SpineCanvas />
 
-      {/* UI Overlay */}
+      {/* UI Panels Overlay */}
       <div className="absolute inset-0 z-10 pointer-events-none">
-        {/* Top Left Panel */}
         <div className="absolute top-5 left-5 pointer-events-auto">
           <MutationLogPanel />
         </div>
-
-        {/* Top Right Panel */}
         <div className="absolute top-5 right-5 pointer-events-auto">
           <SovereignStatusPanel />
         </div>
-
-        {/* Bottom Center Chatbox */}
-        <div className="absolute bottom-5 left-1/2 -translate-x-1/2 pointer-events-auto">
+        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 pointer-events-auto">
           <SovereignTextbox />
         </div>
       </div>

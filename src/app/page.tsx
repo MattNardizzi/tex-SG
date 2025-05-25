8d3e46e4c6fc8a89abb67135bd0d07b877da4910
VortexBlack <matthewnardizzi@Matthews-MacBook-Air.local>
2025-05-24 20:46:04 -0400

    Use alias imports for SpineCanvas and UI panels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,30 @@
 'use client';
 
-import SpineCanvas from '../../components/Spine/SpineCanvas';
-import MutationLogPanel from '../../components/panels/MutationLogPanel';
-import SovereignStatusPanel from '../../components/panels/SovereignStatusPanel';
-import SovereignTextbox from '../../components/ui/SovereignTextbox';
+import SpineCanvas from '@/components/Spine/SpineCanvas';
+import MutationLogPanel from '@/components/panels/MutationLogPanel';
+import SovereignStatusPanel from '@/components/panels/SovereignStatusPanel';
+import SovereignTextbox from '@/components/ui/SovereignTextbox';
 
 export default function MainPage() {
   return (
     <div className="relative w-full h-screen bg-black font-grotesk overflow-hidden">
-      {/* R3F Canvas */}
+      {/* 3D R3F Scene */}
       <SpineCanvas />
 
-      {/* UI Panels */}
+      {/* UI Overlay */}
       <div className="absolute inset-0 z-10 pointer-events-none">
+        {/* Top Left Panel */}
         <div className="absolute top-5 left-5 pointer-events-auto">
           <MutationLogPanel />
         </div>
+
+        {/* Top Right Panel */}
         <div className="absolute top-5 right-5 pointer-events-auto">
           <SovereignStatusPanel />
         </div>
-        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 pointer-events-auto">
+
+        {/* Bottom Center Chatbox */}
+        <div className="absolute bottom-5 left-1/2 -translate-x-1/2 pointer-events-auto">
           <SovereignTextbox />
         </div>
       </div>

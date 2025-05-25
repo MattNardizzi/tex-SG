d638e39a8fcfb0aeb57cb236a21debf950325414
VortexBlack <matthewnardizzi@Matthews-MacBook-Air.local>
2025-05-24 20:30:55 -0400

    Fix final import paths and layout for Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
 'use client';
 
 import { Canvas } from '@react-three/fiber';
-import TexScene from '../../components/_3d/TexScene';
-import MutationLogPanel from '../../components/panels/MutationLogPanel';
-import SovereignStatusPanel from '../../components/panels/SovereignStatusPanel';
-import SovereignTextbox from '../../components/ui/SovereignTextbox';
+import TexScene from '../components/_3d/TexScene';
+import MutationLogPanel from '../components/panels/MutationLogPanel';
+import SovereignStatusPanel from '../components/panels/SovereignStatusPanel';
+import SovereignTextbox from '../components/ui/SovereignTextbox';
 
 export default function MainPage() {
   return (
-    <div className="relative w-full h-screen overflow-hidden bg-black font-grotesk">
-      {/* 3D Canvas Scene */}
+    <div className="relative w-full h-screen bg-black font-grotesk overflow-hidden">
+      {/* 3D Canvas */}
       <Canvas>
         <TexScene />
       </Canvas>
 
-      {/* Overlay UI Panels */}
+      {/* UI Overlay */}
       <div className="absolute inset-0 z-10 pointer-events-none">
-        {/* Top-Left: Mutation Log */}
+        {/* Top Left Panel */}
         <div className="absolute top-5 left-5 pointer-events-auto">
           <MutationLogPanel />
         </div>
 
-        {/* Top-Right: Status Panel */}
+        {/* Top Right Panel */}
         <div className="absolute top-5 right-5 pointer-events-auto">
           <SovereignStatusPanel />
         </div>
 
-        {/* Bottom-Center: Chat Textbox */}
-        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 pointer-events-auto">
+        {/* Bottom Center Chatbox */}
+        <div className="absolute bottom-5 left-1/2 -translate-x-1/2 pointer-events-auto">
           <SovereignTextbox />
         </div>
       </div>

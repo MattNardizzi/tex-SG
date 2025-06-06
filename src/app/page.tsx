fddbbb263d68c841ffda0d3780fb22e446991364
VortexBlack <matthewnardizzi@Matthews-MacBook-Air.local>
2025-06-06 13:19:10 -0400

    🔐 Added password protection with custom wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import Cookies from 'js-cookie';
 import DashboardHUD from '../components/panels/DashboardHUD';
+import withPasswordProtect from '../components/withPassword';
 
-export default function Home() {
-  const router = useRouter();
-  const [showLogout, setShowLogout] = useState(false);
-
-  useEffect(() => {
-    const accessCode = Cookies.get('tex_access_code');
-    if (accessCode) setShowLogout(true);
-  }, []);
-
-  const handleLogout = () => {
-    Cookies.remove('tex_access_code');
-    router.push('/auth');
-  };
-
-  return (
-    <div className="relative w-full h-full">
-      {showLogout && (
-        <button
-          className="absolute top-4 right-4 z-50 bg-red-500 px-4 py-2 rounded hover:bg-red-600"
-          onClick={handleLogout}
-        >
-          Logout
-        </button>
-      )}
-      <DashboardHUD />
-    </div>
-  );
+function Home() {
+  return <DashboardHUD />;
 }
+
+export default withPasswordProtect(Home);
